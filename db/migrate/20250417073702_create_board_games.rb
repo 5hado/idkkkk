24e403b4683cbcade5d59efecd664c82bcdfc62b
Kevin Gibson <ninjamaster0822@gmail.com>
2025-04-30 13:14:27 -0500

    fixed model class

--- db/migrate/20250417073702_create_board_games.rb
+++ db/migrate/20250417073702_create_board_games.rb
@@ -3,10 +3,10 @@ class CreateBoardGames < ActiveRecord::Migration[8.0]
     create_table :board_games do |t|
       t.string :title
       t.string :category
-      t.string :min_player
-      t.string :max_player
+      t.integer :min_player
+      t.integer :max_player
       t.string :publisher
-      t.string :description
+      t.text :description
 
       t.timestamps
     end
